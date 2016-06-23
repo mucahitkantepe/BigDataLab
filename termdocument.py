103c99cc2526b5565e58bbf9a9613a4b5ff45c39
Mucahit Kantepe <mucahitkantepe@gmail.com>
2016-06-23 16:28:46 +0300

    similarity matrix

--- termdocument.py
+++ termdocument.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
 client = MongoClient()
@@ -9,14 +9,12 @@ timeline = db.timeline
 query ={}
 projection = {'text' : 1, '_id' : 0,'user.id' : 1}
 
-
-tweetTextList = []
 tweetDictList=[]
 
 def find(query):
 
     try:
-        cursor = timeline.find({},projection)
+        cursor = timeline.find(query,projection)
 
     except Exception as e:
         print
@@ -27,26 +25,48 @@ def find(query):
         tweetDict['id'] = doc['user']['id']
         tweetDict['text'] = doc['text']
         tweetDictList.append(tweetDict)
-        tweetTextList.append(tweetDict['text'])
-
-
 find(query)
 
-# print(tweetDictList[0]['id'])
-
-
-merged = {}
+# tweet texts merge by user
+def find_index(dicts, key, value):
+    class Null: pass
+    for i, d in enumerate(dicts):
+        if d.get(key, Null) == value:
+            return i
+    else:
+        return -1
+merged = []
 
 for tweet in  tweetDictList:
-    if tweet['id'] not in merged:
-        merged['id'] = tweet['id']
+    tweetDict = {}
+    index1 = find_index(tweetDictList,'id',tweet['id'])
+    index2 = find_index(merged,'id',tweet['id'])
+    #if in  list
+    if index2 >= 0:
+        merged[index2]['text'] += " " + tweet['text']
+    #if not in list
+    else:
+        tweetDict['id'] = tweet['id']
+        tweetDict['text'] = tweet['text']
+        merged.append(tweetDict)
+#print(merged)
 
+vect = TfidfVectorizer(min_df=1)
+tfidf = vect.fit_transform([d['text'] for d in merged])
+terms = np.array(vect.get_feature_names())
+docs = np.asarray(tfidf)
+dtm = np.asarray([docs,terms]).tolist()
+similarityMatrix = (tfidf * tfidf.T).A
+sumdocs = np.sum(docs,axis=0)
+# meandocs = np.mean(docs,axis=0)
+# stddocs = np.std(docs,axis=0)
+np.savetxt("foo.csv", similarityMatrix, delimiter=",")
 
-
-cv = CountVectorizer()
-
-X = cv.fit_transform(tweetTextList) # tüm tweetler textListte.
-terms = np.array(cv.get_feature_names())
-docs = np.asarray(X)# X.sum(axis=0) yaparsan 0'ları siler.
-document_term_tuple = np.asarray([docs,terms]).tolist()
-print(document_term_tuple)
+vect = TfidfVectorizer(min_df=1)
+X = vect.fit_transform([d['text'] for d in tweetDictList])
+terms = np.array(vect.get_feature_names())
+docs = np.asarray(X)
+dtm = np.asarray([docs,terms]).tolist()
+sumdocs = np.sum(docs,axis=0)
+# meandocs = np.mean(docs, axis=0)
+# stddocs = np.std(docs, axis=0)
